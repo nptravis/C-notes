8a4f6c98af0639a81bdba3a7a8ed6158ec3cab2b
nptravis <nptravis@yahoo.com>
2017-11-22 12:23:46 +0700

    insert dictionary entries into a trie

---
+++ dictionary_trie.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <ctype.h>
+#define ALPHABET 26
+
+
+typedef struct node
+{
+	bool is_word;  
+	struct node * child[ALPHABET +1];
+}node;
+
+node * root;
+node * branch;
+node * current;
+
+int main(void)
+{
+	FILE *fp = fopen("dictionary.txt", "r");
+  char word[50];
+  root = malloc(sizeof(node));
+  int j = 0;
+  while(j <= 27){
+  root->child[j] = NULL;
+  j++;
+	}
+
+	while(fscanf(fp, "%s", word) != EOF)
+	{
+		branch = root;
+	int len = strlen(word);
+	printf("%s has %i letters\n", word, len);
+  for(int i=0; i<len; i++)
+  	{
+  		int index = (int)(word[i]) - (int)('a');
+  		printf("%c at index %i of %s\n", word[i], i, word);
+  		printf("the index of the child is: %i\n", index); 
+  		if((i == 0) && (branch->child[index] == NULL))
+  		{
+  			branch = malloc(sizeof(node));
+  			root->child[index] = branch;
+  			branch = root->child[index];
+  			printf("if..\n");
+  		}
+	  	else if(branch->child[index] == NULL)
+	  	{
+	  		current = malloc(sizeof(node));
+	  		branch->child[index] = current;
+	  		branch = current;
+	  			printf("else if..\n");
+	  	}
+	  	else
+	  	{
+	  		branch = branch->child[index];	
+	  			printf("else..\n");
+	  	}
+  	}
+  	branch->is_word = true;
+		
+		printf("this is the end of the word's cycle\n");
+	}
+
+	return 0;
+}
+
--- tester.c
+++ tester.c
@@ -2,98 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdarg.h>
+#include <stdbool.h>
+#include <ctype.h>
 
-// global variable definitions
-int const HASH_MAX = 50;
-
-// node structure
 typedef struct node
 {
-	char word[50];
-	struct node * next;
+	bool is_word;  
+	struct node * child[27];
 }node;
 
-//hash function
-unsigned int hash(char *str)
-{
-	int sum = 0;
-	for( int j = 0; str[j] != '\0'; j++)
-{
-	sum += str[j];
-}
-return sum % HASH_MAX;
-}
-
-node * hashtable[50];
+node * root;
+node * branch;
+node * current;
 
 int main(void)
-{
-	FILE *fp = fopen("dictionary.txt", "r");
-
-	if (fp == NULL)
+{	
+	root = malloc(sizeof(node));
+  root->child[1] = branch;
+	if(root->child[1] == NULL)
 	{
-		printf("Error, can't find/read file\n");
-		return 1;
+		printf("ok...\n");
 	}
-
-	node * temp;
-	node * head;
-	node * p;
-	char word1[50];
-
-	while(fscanf(fp, "%s", word1) != EOF){
-		
-		// create new node
-		temp = malloc(sizeof(node));
-
-		// error check
-		if(temp == NULL)
-		{
-			fclose(fp);
-			printf("Error, unable to malloc new node\n");
-			return 2;
-		}
-
-		// copy word into the node, set it's pointer to null
-		strcpy(temp->word, word1);
-		temp->next = NULL;
-
-		// hash word
-		int hWord = hash(word1);
-
-		// insert word into hashtable according to it's hash code
-		if(hashtable[hWord] == NULL)
-		{
-			hashtable[hWord] = temp;
-			// printf("first word in index location %i, is %s\n", hWord, hashtable[hWord]->word);
-		} 
-		else
-		{
-			temp->next = hashtable[hWord]->next;
-			hashtable[hWord]->next = temp;
-		}
-	}
-
-// displaying members of index locations words linked together
- for(int i = 0; i < HASH_MAX; i++)
- 	{
- 		p = hashtable[i];
- 		printf("Words at location %i are: \n", i);
- 		while(p != NULL)
-			{
-				printf("%s, ", p->word);
-				p = p->next;
-			}
-			printf("\n");
- 	}
-	fclose(fp);
+	
 	return 0;
 }
-
-
-
-
-
-		
-	
-
